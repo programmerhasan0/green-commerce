c6b3d549277b1f6742fd1e8f4ca66f5ff690392e
programerhasan0 <programmerhasan0@gmail.com>
2025-10-29 12:59:58 +0600

    added Dockerfile

--- index.js
+++ index.js
@@ -17,28 +17,27 @@ const app = express();
 const csrfProtection = csrf();
 
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "images");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}_${file.originalname}`);
-  },
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
 });
 const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg" ||
-    file.mimetype === "image/jpeg"
-  ) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
 };
 
 //envirement variables -->
 const uri = process.env.MONGO_URI;
-console.log(uri);
 
 // Global Routes
 const adminRoutes = require("./routes/admin.route");
@@ -53,8 +52,8 @@ const UserModel = require("./models/user.model");
 
 // working with sessions {connect-mongodb-session => store}
 const store = new MongoDBStore({
-  uri,
-  collection: "sessions",
+    uri,
+    collection: "sessions",
 });
 //setting the view engine and view directory
 app.set("view engine", "ejs");
@@ -63,43 +62,43 @@ app.set("views", "./views");
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+    multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/images", express.static(path.join(__dirname, "images")));
 app.use(
-  session({
-    secret: "xZI9M57QreeUopF0",
-    resave: false,
-    saveUninitialized: false,
-    store,
-  })
+    session({
+        secret: "xZI9M57QreeUopF0",
+        resave: false,
+        saveUninitialized: false,
+        store,
+    })
 );
 app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.isLoggedIn = req.session.isLoggedIn;
-  res.locals.csrfToken = req.csrfToken();
-  next();
+    res.locals.isLoggedIn = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  UserModel.findById(req.session.user._id)
-    .then((user) => {
-      if (!user) {
+    if (!req.session.user) {
         return next();
-      }
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      errorHandler.handle500(err, next);
-    });
+    }
+    UserModel.findById(req.session.user._id)
+        .then(user => {
+            if (!user) {
+                return next();
+            }
+            req.user = user;
+            next();
+        })
+        .catch(err => {
+            errorHandler.handle500(err, next);
+        });
 });
 
 //routes
@@ -115,8 +114,8 @@ const port = process.env.PORT || 3000;
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useUnifiedTopology", true);
 mongoose
-  .connect(uri)
-  .then((result) => {
-    app.listen(port, () => console.log(`listening from port ${port}`));
-  })
-  .catch((err) => console.log(err));
+    .connect(uri)
+    .then(result => {
+        app.listen(port, () => console.log(`listening from port ${port}`));
+    })
+    .catch(err => console.log(err));
